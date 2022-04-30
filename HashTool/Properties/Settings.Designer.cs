b5a28a2147b96dbd1460aa2d1a800ba20ff5f064
杨可 <KiyanYang@outlook.com>
2022-04-30 15:03:06 +0800

    ⚙️ 更新设置中选择的算法

--- HashTool/Properties/Settings.Designer.cs
+++ HashTool/Properties/Settings.Designer.cs
@@ -25,20 +25,15 @@ namespace HashTool.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute(@"<?xml version=""1.0"" encoding=""utf-16""?>
-<ArrayOfString xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"">
-  <string>CRC32</string>
-  <string>MD4</string>
-  <string>MD5</string>
-  <string>SHA1</string>
-  <string>SHA256</string>
-</ArrayOfString>")]
-        public global::System.Collections.Specialized.StringCollection SelectedHashAlgorithm {
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsd=\"http://www.w3." +
+            "org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\r\n  <s" +
+            "tring>MD5</string>\r\n  <string>SHA256</string>\r\n</ArrayOfString>")]
+        public global::System.Collections.Specialized.StringCollection SelectedHashAlgorithms {
             get {
-                return ((global::System.Collections.Specialized.StringCollection)(this["SelectedHashAlgorithm"]));
+                return ((global::System.Collections.Specialized.StringCollection)(this["SelectedHashAlgorithms"]));
             }
             set {
-                this["SelectedHashAlgorithm"] = value;
+                this["SelectedHashAlgorithms"] = value;
             }
         }
         
